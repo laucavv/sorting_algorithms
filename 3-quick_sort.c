f8376bf43120dc13cb28db6c81bba9d90bef86b7
laucavv <laucavv23@gmail.com>
2020-06-15 20:35:34 +0000

    quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,11 +1,19 @@
 #include "sort.h"
+/**
+ * lomotu_partition - lomotu partition scheme
+ * @array: array of numbers
+ * @low: first index of the array
+ * @high: last index of the array
+ * @size: size of the array
+ * Return: position of the pivot in the array
+ */
 
-int lomotu_partition(int *array, int low, int hight, size_t size)
+int lomotu_partition(int *array, int low, int high, size_t size)
 {
-	int pi = low, pivot = array[hight], i = low, tmp;
+	int pi = low, pivot = array[high], i = low, tmp;
 
 
-	for (; i < hight; i++)
+	for (; i < high; i++)
 	{
 		if (array[i] < pivot)
 		{
@@ -20,8 +28,8 @@ int lomotu_partition(int *array, int low, int hight, size_t size)
 		}
 	}
 	tmp = array[pi];
-	array[pi] = array[hight];
-	array[hight] = tmp;
+	array[pi] = array[high];
+	array[high] = tmp;
 	if (i != pi)
 	{
 		print_array(array, size);
@@ -29,22 +37,36 @@ int lomotu_partition(int *array, int low, int hight, size_t size)
 	return (pi);
 }
 
-
-void quick_sort_rec(int *array, int low, int hight, size_t size)
+/**
+ * quick_sort_rec - recursive call to partition function
+ * @array: array of numbers
+ * @low: first index of the array
+ * @high: last index of the array
+ * @size: size of the array
+ * Return: nothing
+ */
+void quick_sort_rec(int *array, int low, int high, size_t size)
 {
-        int pivot = 0;
+	int pivot = 0;
 
-        if (low < hight)
-        {
-                pivot = lomotu_partition(array, low, hight, size);
-                quick_sort_rec(array, low, pivot - 1, size);
-                quick_sort_rec(array, pivot + 1, hight, size);
-        }
+	if (low < high)
+	{
+		pivot = lomotu_partition(array, low, high, size);
+		quick_sort_rec(array, low, pivot - 1, size);
+		quick_sort_rec(array, pivot + 1, high, size);
+	}
 }
+/**
+ * quick_sort - sorts an array of integers in
+ * ascending order using the Quick sort algorithm
+ * @array: array to be sorted
+ * @size: size of the array
+ * Return: nothing
+ */
 
 void quick_sort(int *array, size_t size)
 {
-        if (array == NULL || size < 2)
-                return;
-        quick_sort_rec(array, 0, size - 1, size);
+	if (array == NULL || size < 2)
+		return;
+	quick_sort_rec(array, 0, size - 1, size);
 }
